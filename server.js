28af38a3a9f6cb69319b1d227c8dba02ced34b71
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-11-06 00:26:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(
 
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://docsync-server-w3s5.onrender.com',
+    origin: 'https://docsync-client-6z30.onrender.com',
     methods: ['GET', 'POST'],
   },
 });
